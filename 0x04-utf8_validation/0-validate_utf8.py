3c90e755be31f5fd69e68c189e28aa26f9684727
Solomon <113886983+solomongegziabher@users.noreply.github.com>
2023-08-24 23:35:02 +0300

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,34 +1,39 @@
 #!/usr/bin/python3
-"""
-Write a method that determines if a given data set represents
-a valid UTF-8 encoding.
-  - Prototype: def validUTF8(data)
-  - Return: True if data is a valid UTF-8 encoding, else return False
-  - A character in UTF-8 can be 1 to 4 bytes long
-  - The data set can contain multiple characters
-  - The data will be represented by a list of integers
-  - Each integer represents 1 byte of data, therefore you only
-    need to handle the 8 least significant bits of each integer
- """
+"""UTF-8 Validation"""
 
 
 def validUTF8(data):
-    """ determines if a given set represents a valid
-    UTF-8 encoding
+    """Determines if a given data set
+    represents a valid utf-8 encoding
     """
-    count = 0
-    for c in data:
-        if count == 0:
-            if (c >> 5) == 0b110:
-                count = 1
-            elif (c >> 4) == 0b1110:
-                count = 2
-            elif (c >> 3) == 0b11110:
-                count = 3
-            elif c >> 7:
+    number_bytes = 0
+
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
                 return False
+
         else:
-            if (c >> 6) != 0b10:
+            if not (i & mask_1 and not (i & mask_2)):
                 return False
-            count -= 1
-    return count == 0
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
