493406dd3a1d8c26dcd34e53ad3dd27ed999fc8b
Solomon <113886983+solomongegziabher@users.noreply.github.com>
2023-08-31 14:07:00 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,134 +1,67 @@
 #!/usr/bin/python3
-"""Solves the N-queens puzzle.
-Determines all possible solutions to placing N
-N non-attacking queens on an NxN chessboard.
-Example:
-    $ ./101-nqueens.py N
-N must be an integer greater than or equal to 4.
-Attributes:
-    board (list): A list of lists representing the chessboard.
-    solutions (list): A list of lists containing solutions.
-Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
-where `r` and `c` represent the row and column, respectively, where a
-queen must be placed on the chessboard.
-"""
-import sys
+from queue import Queue
 
 
-def init_board(n):
-    """Initialize an `n`x`n` sized chessboard with 0's."""
-    board = []
-    [board.append([]) for i in range(n)]
-    [row.append(' ') for i in range(n) for row in board]
-    return (board)
+class NQueens:
+"""Initialize an `n`x`n` sized chessboard with 0's."""
+    def __init__(self, size):
+        self.size = size
 
+    def solve_dfs(self):
+        if self.size < 1:
+            return []
+        solutions = []
+        stack = [[]]
+        while stack:
+            solution = stack.pop()
+            if self.conflict(solution):
+                continue
+            row = len(solution)
+            if row == self.size:
+                solutions.append(solution)
+                continue
+            for col in range(self.size):
+                queen = (row, col)
+                queens = solution.copy()
+                queens.append(queen)
+                stack.append(queens)
+        return solutions
 
-def board_deepcopy(board):
-    """Return a deepcopy of a chessboard."""
-    if isinstance(board, list):
-        return list(map(board_deepcopy, board))
-    return (board)
+    def solve_bfs(self):
+        if self.size < 1:
+            return []
+        solutions = []
+        queue = Queue()
+        queue.put([])
+        while not queue.empty():
+            solution = queue.get()
+            if self.conflict(solution):
+                continue
+            row = len(solution)
+            if row == self.size:
+                solutions.append(solution)
+                continue
+            for col in range(self.size):
+                queen = (row, col)
+                queens = solution.copy()
+                queens.append(queen)
+                queue.put(queens)
+        return solutions
 
+    def conflict(self, queens):
+        for i in range(1, len(queens)):
+            for j in range(0, i):
+                a, b = queens[i]
+                c, d = queens[j]
+                if a == c or b == d or abs(a - c) == abs(b - d):
+                    return True
+        return False
 
-def get_solution(board):
-    """Return the list of lists representation of a solved chessboard."""
-    solution = []
-    for r in range(len(board)):
-        for c in range(len(board)):
-            if board[r][c] == "Q":
-                solution.append([r, c])
-                break
-    return (solution)
-
-
-def xout(board, row, col):
-    """X out spots on a chessboard.
-    All spots where non-attacking queens can no
-    longer be played are X-ed out.
-    Args:
-        board (list): The current working chessboard.
-        row (int): The row where a queen was last played.
-        col (int): The column where a queen was last played.
-    """
-    # X out all forward spots
-    for c in range(col + 1, len(board)):
-        board[row][c] = "x"
-    # X out all backwards spots
-    for c in range(col - 1, -1, -1):
-        board[row][c] = "x"
-    # X out all spots below
-    for r in range(row + 1, len(board)):
-        board[r][col] = "x"
-    # X out all spots above
-    for r in range(row - 1, -1, -1):
-        board[r][col] = "x"
-    # X out all spots diagonally down to the right
-    c = col + 1
-    for r in range(row + 1, len(board)):
-        if c >= len(board):
-            break
-        board[r][c] = "x"
-        c += 1
-    # X out all spots diagonally up to the left
-    c = col - 1
-    for r in range(row - 1, -1, -1):
-        if c < 0:
-            break
-        board[r][c]
-        c -= 1
-    # X out all spots diagonally up to the right
-    c = col + 1
-    for r in range(row - 1, -1, -1):
-        if c >= len(board):
-            break
-        board[r][c] = "x"
-        c += 1
-    # X out all spots diagonally down to the left
-    c = col - 1
-    for r in range(row + 1, len(board)):
-        if c < 0:
-            break
-        board[r][c] = "x"
-        c -= 1
-
-
-def recursive_solve(board, row, queens, solutions):
-    """Recursively solve an N-queens puzzle.
-    Args:
-        board (list): The current working chessboard.
-        row (int): The current working row.
-        queens (int): The current number of placed queens.
-        solutions (list): A list of lists of solutions.
-    Returns:
-        solutions
-    """
-    if queens == len(board):
-        solutions.append(get_solution(board))
-        return (solutions)
-
-    for c in range(len(board)):
-        if board[row][c] == " ":
-            tmp_board = board_deepcopy(board)
-            tmp_board[row][c] = "Q"
-            xout(tmp_board, row, c)
-            solutions = recursive_solve(tmp_board, row + 1,
-                                        queens + 1, solutions)
-
-    return (solutions)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    if sys.argv[1].isdigit() is False:
-        print("N must be a number")
-        sys.exit(1)
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    board = init_board(int(sys.argv[1]))
-    solutions = recursive_solve(board, 0, 0, [])
-    for sol in solutions:
-        print(sol)
+    def print(self, queens):
+        for i in range(self.size):
+            print(' ---' * self.size)
+            for j in range(self.size):
+                p = 'Q' if (i, j) in queens else ' '
+                print('| %s ' % p, end='')
+            print('|')
+        print(' ---' * self.size)
