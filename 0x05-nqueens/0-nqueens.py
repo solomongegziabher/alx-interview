f99bd0a42627b3b73d334f4ca56cdabd895a13ed
Solomon <113886983+solomongegziabher@users.noreply.github.com>
2023-08-31 14:13:11 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,67 +1,73 @@
 #!/usr/bin/python3
-from queue import Queue
+import sys
 
+def is_safe(board, row, col):
+    # Check if a queen can be placed at board[row][col]
+   
+    # Check upper row
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
 
-class NQueens:
-"""Initialize an `n`x`n` sized chessboard with 0's."""
-    def __init__(self, size):
-        self.size = size
+    # Check upper left diagonal
+    i, j = row, col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
 
-    def solve_dfs(self):
-        if self.size < 1:
-            return []
-        solutions = []
-        stack = [[]]
-        while stack:
-            solution = stack.pop()
-            if self.conflict(solution):
-                continue
-            row = len(solution)
-            if row == self.size:
-                solutions.append(solution)
-                continue
-            for col in range(self.size):
-                queen = (row, col)
-                queens = solution.copy()
-                queens.append(queen)
-                stack.append(queens)
-        return solutions
+    # Check upper right diagonal
+    i, j = row, col
+    while i >= 0 and j < N:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j += 1
 
-    def solve_bfs(self):
-        if self.size < 1:
-            return []
-        solutions = []
-        queue = Queue()
-        queue.put([])
-        while not queue.empty():
-            solution = queue.get()
-            if self.conflict(solution):
-                continue
-            row = len(solution)
-            if row == self.size:
-                solutions.append(solution)
-                continue
-            for col in range(self.size):
-                queen = (row, col)
-                queens = solution.copy()
-                queens.append(queen)
-                queue.put(queens)
-        return solutions
+    return True
 
-    def conflict(self, queens):
-        for i in range(1, len(queens)):
-            for j in range(0, i):
-                a, b = queens[i]
-                c, d = queens[j]
-                if a == c or b == d or abs(a - c) == abs(b - d):
-                    return True
-        return False
+def solve_nqueens(board, row):
+    # Base case: All queens placed, print current configuration
+    if row == N:
+        print(board)
+        return
 
-    def print(self, queens):
-        for i in range(self.size):
-            print(' ---' * self.size)
-            for j in range(self.size):
-                p = 'Q' if (i, j) in queens else ' '
-                print('| %s ' % p, end='')
-            print('|')
-        print(' ---' * self.size)
+    # Try placing queen in each column of current row
+    for col in range(N):
+        if is_safe(board, row, col):
+            # Place queen at board[row][col]
+            board[row][col] = 1
+
+            # Recurse on the next row
+            solve_nqueens(board, row + 1)
+
+            # Backtrack: Remove queen from board[row][col]
+            board[row][col] = 0
+
+def nqueens(N):
+    # Create an empty NxN board
+    board = [[0] * N for _ in range(N)]
+
+    # Solve the N Queens problem
+    solve_nqueens(board, 0)
+
+# Check if the correct number of arguments is provided
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+# Get the value of N from command line argument
+try:
+    N = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
+
+# Check if N is at least 4
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+
+# Solve and print the N Queens problem
+nqueens(N)
