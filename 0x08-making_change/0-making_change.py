fb2aec06ac1ec57e766edefc519d1bfbe11eac6c
Solomon <113886983+solomongegziabher@users.noreply.github.com>
2023-09-20 10:59:31 +0300

    Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,15 +1,43 @@
 #!/usr/bin/python3
-""" Change comes from within
+"""
+Making Change
 """
 
 
 def makeChange(coins, total):
-    """ Return the fewest number of coins needed to meet total
     """
-    mx = total + 1
-    dp = [0] + [mx for _ in range(total)]
-    for i in range(1, len(dp)):
-        for c in coins:
-            if i - c >= 0:
-                dp[i] = min(dp[i], dp[i - c] + 1)
-    return dp[-1] if dp[-1] != total + 1 else - 1
+    Return the minimum number of coins needed to meet a given total
+    Args:
+        coins (list of ints): a list of coins of different values
+        total (int): total value to be met
+    Return:
+        Number of coins or -1 if meeting the total is not possible
+    """
+    if total <= 0:
+        return 0
+    if coins == [] or coins is None:
+        return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
+
+    coins.sort(reverse=True)
+    coin_count = 0
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
--- 0x08-making_change/README.md
+++ 0x08-making_change/README.md
@@ -1,1 +1,1 @@
-### 0x08. Making Change
+# Make a change
