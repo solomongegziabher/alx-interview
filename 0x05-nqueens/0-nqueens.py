4e5a8b4f34aa75a2bd86e33e3f6c83af6cb23479
Solomon <113886983+solomongegziabher@users.noreply.github.com>
2023-09-29 23:05:46 +0300

    NQueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,68 +1,51 @@
 #!/usr/bin/python3
+
+"""program that solves the N queens problem
+Usage: nqueens N
+where N must be an integer greater or equal to 4
+The program should print everrow possible solution to the problem
 """
-Solution to the nqueens problem
-"""
-import sys
+
+from sys import argv
 
 
-def backtrack(r, n, cols, pos, neg, board):
-    """
-    backtrack function to find solution
-    """
-    if r == n:
-        res = []
-        for l in range(len(board)):
-            for k in range(len(board[l])):
-                if board[l][k] == 1:
-                    res.append([l, k])
-        print(res)
-        return
+def queens_potitions(Nq, row, queens):
 
-    for c in range(n):
-        if c in cols or (r + c) in pos or (r - c) in neg:
-            continue
+    for column in range(Nq):
+        flag = 0
 
-        cols.add(c)
-        pos.add(r + c)
-        neg.add(r - c)
-        board[r][c] = 1
+        for queen in queens:
 
-        backtrack(r+1, n, cols, pos, neg, board)
+            if abs(column - queen[1]) == abs(row - queen[0]):
+                flag = 1
+                break
 
-        cols.remove(c)
-        pos.remove(r + c)
-        neg.remove(r - c)
-        board[r][c] = 0
+            if column == queen[1]:
+                flag = 1
+                break
 
-
-def nqueens(n):
-    """
-    Solution to nqueens problem
-    Args:
-        n (int): number of queens. Must be >= 4
-    Return:
-        List of lists representing coordinates of each
-        queen for all possible solutions
-    """
-    cols = set()
-    pos_diag = set()
-    neg_diag = set()
-    board = [[0] * n for i in range(n)]
-
-    backtrack(0, n, cols, pos_diag, neg_diag, board)
-
+        if flag == 0:
+            queens.append([row, column])
+            if row != Nq - 1:
+                queens_potitions(Nq, row + 1, queens)
+            else:
+                print(queens)
+            queens.pop()
 
 if __name__ == "__main__":
-    n = sys.argv
-    if len(n) != 2:
+    """receive console arguments and call function"""
+
+    if len(argv) < 2:
         print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        nn = int(n[1])
-        if nn < 4:
-            print("N must be at least 4")
-            sys.exit(1)
-        nqueens(nn)
-    except ValueError:
+        exit(1)
+    if argv[1].isdigit() is False:
         print("N must be a number")
-        sys.exit(1)
+        exit(1)
+    Nq = int(argv[1])
+    if Nq < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    row = 0
+    queens = []
+    queens_potitions(Nq, row, queens)
